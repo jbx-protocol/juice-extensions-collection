5dc997efc38c70f13e9676818cd7eccb743ce7ef
mejango <mejango@protonmail.com>
2022-04-25 23:38:54 -0400

    first

---
+++ contracts/DataSourceDelegate.sol
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.6;
+
+import '@jbx-protocol/contracts-v2/contracts/interfaces/IJBFundingCycleDataSource.sol';
+import '@jbx-protocol/contracts-v2/contracts/interfaces/IJBPayDelegate.sol';
+
+contract DataSourceDelegate is IJBFundingCycleDataSource, IJBPayDelegate {
+  function payParams(JBPayParamsData calldata _data)
+    external
+    view
+    override
+    returns (
+      uint256 weight,
+      string memory memo,
+      IJBPayDelegate delegate
+    )
+  {
+    return (0, _data.memo, IJBPayDelegate(address(this)));
+  }
+
+  function redeemParams(JBRedeemParamsData calldata)
+    external
+    view
+    override
+    returns (
+      uint256 reclaimAmount,
+      string memory memo,
+      IJBRedemptionDelegate delegate
+    )
+  {
+    return (0, '', IJBRedemptionDelegate(address(0)));
+  }
+
+  function didPay(JBDidPayData calldata _data) external override {}
+
+  function supportsInterface(bytes4 _interfaceId) external pure override returns (bool) {
+    return
+      _interfaceId == type(IJBFundingCycleDataSource).interfaceId ||
+      _interfaceId == type(IJBPayDelegate).interfaceId;
+  }
+}
