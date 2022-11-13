14849e065dd10f6d142392f4db5c239f4dd01aeb
viraj124 <virajm72@gmail.com>
2022-11-13 09:42:22 +0530

    feat: v1 to v3 allocator

--- contracts/Allocator/V1Allocator.sol
+++ contracts/Allocator/V1Allocator.sol
@@ -1,37 +1,33 @@
-// // SPDX-License-Identifier: MIT
-// pragma solidity 0.8.6;
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.6;
 
-// import '@openzeppelin/contracts/utils/introspection/ERC165.sol';
-// import '@jbx-protocol-v1/contracts/interfaces/IModAllocator.sol';
+import '@openzeppelin/contracts/utils/introspection/ERC165.sol';
+import '@jbx-protocol-v1/contracts/interfaces/IModAllocator.sol';
 
-// /**
-//  @title
-//  Juicebox split allocator
+/**
+ @title
+ Juicebox split allocator for allocating v1 treasury funds to v3 treasury
+*/
+contract V1Allocator is ERC165, IModAllocator {
+  error TRANSACTIONAL_TOKEN_TRANSFER_FAILURE();
+  function allocate(uint256 _projectId, uint256 _forProjectId, address _beneficiary) external payable override {
+    // avoid compiler warnings
+    _projectId; 
+    _forProjectId;
+    // send eth to the beneficiary
+    (bool success, ) = _beneficiary.call{ value: msg.value, gas: 20000 }("");
+    if (!success) {
+        revert TRANSACTIONAL_TOKEN_TRANSFER_FAILURE();
+    }
+  }
 
-//  @notice
-//  This is an allocator template, used as a recipient of a payout split, to add an extra layer of logic in fund allocation
-// */
-// contract V1Allocator is ERC165, IModAllocator {
-
-//   function allocate(uint256 _projectId, uint256 _forProjectId, address _beneficiary) external payable override {
-//     // Do something with the fund received
-    
-//     // if (_data.token == JBTokens.ETH) {
-//     //   _data.split.beneficiary.call({value: msg.value, gas: 3000});
-//     // } else if (tokenStore.tokenOf(_ddata.projectId) != _data.token) {
-//     //   IERC20(_data.token).transferFrom(msg.sender, _data.split.beneficiary, _data.amount);
-//     // } else {
-//     //   tokenStore.trans
-//     // }
-//   }
-
-//   function supportsInterface(bytes4 _interfaceId)
-//     public
-//     view
-//     override(IERC165, ERC165)
-//     returns (bool)
-//   {
-//     return
-//       _interfaceId == type(IJBSplitAllocator).interfaceId || super.supportsInterface(_interfaceId);
-//   }
-// }
+  function supportsInterface(bytes4 _interfaceId)
+    public
+    view
+    override
+    returns (bool)
+  {
+    return
+      _interfaceId == type(IModAllocator).interfaceId || super.supportsInterface(_interfaceId);
+  }
+}
